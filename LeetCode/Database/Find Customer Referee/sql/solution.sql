11c74857036d47f4448f58ce43b6c4462346cd7e
Jeeva S <jeevajeeva1672@gmail.com>
2026-08-20 09:59:20 +0530

    feat: Add Find Customer Referee

--- LeetCode/Database/Find Customer Referee/sql/solution.sql
+++ LeetCode/Database/Find Customer Referee/sql/solution.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
-select name
-from Customer
-where referee_id is NULL or referee_id !=2;
+# Write your MySQL query statement below
+select name
+from Customer
+where referee_id is NULL or referee_id !=2;
